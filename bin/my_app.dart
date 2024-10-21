91d6c9739a6bd1a104681e209a7db13f74be9e79
Andreia Lira <andreia.s.lira08@gmail.com>
2024-10-21 10:14:14 -0300

    Praticando conceitos de abstração

--- bin/my_app.dart
+++ bin/my_app.dart
@@ -1,13 +1,7 @@
-import 'package:my_app/classes/Pessoa.dart';
 import 'package:my_app/classes/PessoaFisica.dart';
 import 'package:my_app/classes/Animal.dart';
 
 void main(List<String> arguments) {
-  Pessoa p1 = Pessoa('Ana', 30);
-
-  print(p1.getNome());
-  print(p1.getIdade());
-
   Animal a1 = Animal();
   Animal gato = Gato();
 
